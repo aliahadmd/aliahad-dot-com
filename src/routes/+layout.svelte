f68cbfa1e8c7a1fcb6af8cd154d37f34db72d6d0
ali <iamaliahad@outlook.com>
2023-04-03 08:16:29 +0800

    add github icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
 	import '../app.css';
 </script>
 
-<div class="text-center pt-3">
+<div class="text-center pt-3 sticky top-0 z-50">
 	<a class="p-1" href="/">Home</a>
 	<a class="p-1" href="/contact">Contact</a>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,20 @@
-<div class="pt-6">
-	<h1 class="text-center text-3xl font-bold">Ali Ahad</h1>
-	<h3 class="text-center text-xl">Full Stack Developer!</h3>
+<script>
+	import Github from '../component/icon/Github.svelte';
+</script>
+
+<div class="flex h-screen flex-col">
+	<div class="pt-6">
+		<h1 class="text-center text-3xl font-bold">Ali Ahad</h1>
+		<h3 class="text-center text-xl">Full Stack Developer!</h3>
+	</div>
+	<div class="flex justify-center p-1">
+		<a class="p-1" href="https://www.github.com/iamAliahad">
+			<Github />
+		</a>
+	</div>
 </div>
+
+<!-- you can pass class (or anything else) using $$restProps -->
 
 <style lang="postcss">
 	:global(html) {
